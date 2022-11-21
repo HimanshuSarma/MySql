113da5a75087427dee8450e958a84f2342544c9a
Himanshu Sharma <sweetiehimanshu@gmail.com>
2022-11-21 21:14:51 +0530

    new

--- 586.Customer_Placing_the_Largest_Number_of_Orders.sql
+++ 586.Customer_Placing_the_Largest_Number_of_Orders.sql
@@ -1,9 +1,12 @@
-select customer_number from Orders 
-group by customer_number
-having count(order_number) = (
-    select max(total_orders) from (
-        select count(order_number) as total_orders, customer_number
-        from Orders as Customer_total_orders
-        group by customer_number
-    ) Customer_total_orders
+with cte as (
+    select count(order_number) as total_orders, customer_number
+    from Orders as Customer_total_orders
+    group by customer_number
+)
+
+select customer_number from cte
+where total_orders = (
+    select max(total_orders) from cte
 );
+
+
